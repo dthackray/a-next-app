63410c532ea9849d347e434740456bc4119f2076
Dan Thackray <djthackray@gmail.com>
2022-09-15 14:20:13 +0100

    Amend tailwind.config.js
    Signed-off-by: Dan Thackray <djthackray@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+   darkMode: class,
+   theme: {
+     extend: {},
+   },
+   variants: {
+     extend: {},
+   },
+   plugins: [],
+ }
